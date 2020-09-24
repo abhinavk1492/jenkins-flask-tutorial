5ba93bf30c47fd252c13c09dc3e86f16f40cf470
abhinavk1492 <35155656+abhinavk1492@users.noreply.github.com>
2020-09-24 19:51:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler())
+app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def hello_world():
+    app.logger.warning('inside hello_world method')
     return 'Flask Dockerized 7.0'
 
 if __name__ == '__main__':
